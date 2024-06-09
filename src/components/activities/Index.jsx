7c9b2886a152d48cbf01c674ad5a4fdaedccdd64
Zvki1 <rzaki@hotmail.fr>
2024-06-09 16:58:42 +0100

    chore: Refactor activities/Index.jsx for improved readability and consistency

--- src/components/activities/Index.jsx
+++ src/components/activities/Index.jsx
@@ -1,17 +1,19 @@
-import subtitle from "../../assets/Activities/subtitle.svg"
-import {ActivitiesCarousel} from "./ActivitiesCarousel"
+import subtitle from "../../assets/Activities/subtitle.svg";
+import { ActivitiesCarousel } from "./ActivitiesCarousel";
 const Index = () => {
   return (
     <div className="flex items-center flex-col py-11 bg-gradient-to-b from-[#35506F] to-[#243245]">
-       <div className="flex flex-col items-center justify-center gap-2  ">
-          <h3 className="text-white font-Tajawal text-5xl font-[800]  ">
+      <div className="flex flex-col items-center justify-center   ">
+        <h3 className="text-white font-Tajawal text-5xl font-[800]  ">
           نـــشـــاطــــــتنــــــــــا
-          </h3>
-          <img src={subtitle} alt="subtitle" className="w-full " />
-        </div>
+        </h3>
+        <img src={subtitle} alt="subtitle" className="w-full " />
+      </div>
+      <div className="md:hidden">
         <ActivitiesCarousel />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
